e45abb073ae643ff72f0867da57f242f00f51bf6
Sujal <sujal.kumar311@gmail.com>
2024-05-10 16:31:52 +0530

    upload files to cloudinary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,8 +1,58 @@
 import { NextFunction, Request, Response } from "express";
 import Book from "./bookModel";
+import cloudinary from "../config/cloudinary";
+import path from "node:path";
+import createHttpError from "http-errors";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    console.log("Files", req.files);
+
+    // this is telling the type of the file
+    const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+
+    // application/pdf
+    const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+
+    const fileName = files.coverImage[0].filename;
+
+    const filePath = path.resolve(
+      __dirname,
+      "../../public/data/uploads",
+      fileName
+    );
+
+    try {
+      const uploadResult = await cloudinary.uploader.upload(filePath, {
+        filename_override: fileName,
+        folder: "book-covers",
+        format: coverImageMimeType,
+      });
+
+      const bookFileName = files.file[0].filename;
+      const bookFilePath = path.resolve(
+        __dirname,
+        "../../public/data/uploads",
+        bookFileName
+      );
+
+      const bookFileUploadResult = await cloudinary.uploader.upload(
+        bookFilePath,
+        {
+          resource_type: "raw",
+          filename_override: bookFileName,
+          folder: "book-pdfs",
+          foramt: "pdf",
+        }
+      );
+      console.log(bookFileUploadResult);
+      console.log("upload result", uploadResult);
+    } catch (error) {
+      console.log(error);
+      return next(createHttpError(500, "Error while uploading the files."));
+    }
+
+    res.json({});
   } catch (error) {}
 };
 
--- src/config/config.ts
+++ src/config/config.ts
@@ -7,6 +7,9 @@ const _config = {
   database_uri: process.env.MONGO_URI,
   env: process.env.NODE_ENV,
   jwtSecret: process.env.JWT_SECRET,
+  cloudName: process.env.CLOUDINARY_CLOUD,
+  cloudinaryapikey: process.env.CLOUDINARY_API_KEY,
+  cloudinarysecretkey: process.env.CLOUDINARY_API_SECRET,
 };
 
 export const config = Object.freeze(_config);
